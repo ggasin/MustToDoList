aad550a51bf3a59fc420f167c7848790c95980e3
류승민 <tmdals028@naver.com>
2023-06-14 23:34:31 +0900

    지난 할일 목록 activity 구성 진행중

---
+++ app/src/main/java/com/example/musttodolist/DoneTodoListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.musttodolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoneTodoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_done_todo_list)
+    }
+}
